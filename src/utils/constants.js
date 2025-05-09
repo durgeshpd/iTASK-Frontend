0a3d64706c3295828381419abf11cc054e05bed4
Durgesh Pandey <41140945+durgeshpd@users.noreply.github.com>
2025-05-09 22:15:02 +0530

    update BASE_URL

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,4 @@
 export const BASE_URL =
-    location.hostname === "localhost" ? "http://localhost:3000" : "/api";
+    location.hostname === "localhost"
+        ? "http://localhost:3001"
+        : "/itask-api";
